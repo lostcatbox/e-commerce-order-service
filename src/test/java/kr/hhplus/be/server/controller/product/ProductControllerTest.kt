3f0011cccebb75a0235ff4ec47fb50460bf69565
lostcatbox <lostcatbox@gmail.com>
2025-08-27 11:16:14 +0900

    [test] Coupon, Product에 대한 MockApi Test 추가 구현

--- src/test/java/kr/hhplus/be/server/controller/product/ProductControllerTest.kt
+++ src/test/java/kr/hhplus/be/server/controller/product/ProductControllerTest.kt
@@ -12,19 +12,34 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 @WebMvcTest(ProductController::class)
 class ProductControllerTest {
+    @Autowired
+    lateinit var mockMvc: MockMvc
 
-	@Autowired
-	lateinit var mockMvc: MockMvc
+    @DisplayName("인기 상품 목록을 조회한다.")
+    @Test
+    fun getPopularProducts() {
+        mockMvc
+            .perform(
+                get("/api/v1/products/popular"),
+            ).andDo(print())
+            .andExpect(status().isOk)
+            .andExpect(jsonPath("$.products").isArray)
+            .andExpect(jsonPath("$.generatedAt").isNumber)
+    }
 
-	@DisplayName("인기 상품 목록을 조회한다.")
-	@Test
-	fun getPopularProducts() {
-		mockMvc.perform(
-			get("/api/v1/products/popular")
-		)
-			.andDo(print())
-			.andExpect(status().isOk)
-			.andExpect(jsonPath("$.products").isArray)
-			.andExpect(jsonPath("$.generatedAt").isNumber)
-	}
+    @DisplayName("상품 상세 정보를 조회한다.")
+    @Test
+    fun getProduct() {
+        val productId = 1L
+        mockMvc
+            .perform(
+                get("/api/v1/products/{productId}", productId),
+            ).andDo(print())
+            .andExpect(status().isOk)
+            .andExpect(jsonPath("$.productId").value(productId))
+            .andExpect(jsonPath("$.name").isString)
+            .andExpect(jsonPath("$.description").isString)
+            .andExpect(jsonPath("$.price").isNumber)
+            .andExpect(jsonPath("$.stock").isNumber)
+    }
 }
