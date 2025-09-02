3205fabf929d1c7c224b19b25f0067cd410006c1
lostcatbox <lostcatbox@gmail.com>
2025-09-02 21:36:40 +0900

    [feat] 유저 포인트 도메인 인프라 레이어 구현

---
+++ src/main/kotlin/kr/hhplus/be/server/infra/persistence/point/UserPointRepositoryImpl.kt
@@ -0,0 +1,19 @@
+package kr.hhplus.be.server.infra.persistence.point
+
+import kr.hhplus.be.server.core.point.domain.UserPoint
+import kr.hhplus.be.server.core.point.repository.UserPointRepository
+import kr.hhplus.be.server.infra.persistence.point.jpa.JpaUserPointRepository
+import org.springframework.stereotype.Repository
+
+/**
+ * 사용자 포인트 Repository 구현체
+ */
+@Repository
+class UserPointRepositoryImpl(
+    private val jpaUserPointRepository: JpaUserPointRepository,
+) : UserPointRepository {
+
+    override fun findByUserId(userId: Long): UserPoint? = jpaUserPointRepository.findByUserId(userId)
+
+    override fun save(userPoint: UserPoint): UserPoint = jpaUserPointRepository.save(userPoint)
+}
