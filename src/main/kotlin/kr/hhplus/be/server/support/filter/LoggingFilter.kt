ff5631faf77955189d99485c1116bbd96836c95e
lostcatbox <lostcatbox@gmail.com>
2025-09-09 17:18:01 +0900

    [feat] LoggingFilter.kt 추가

---
+++ src/main/kotlin/kr/hhplus/be/server/support/filter/LoggingFilter.kt
@@ -0,0 +1,74 @@
+package kr.hhplus.be.server.support.filter
+
+import jakarta.servlet.FilterChain
+import jakarta.servlet.ServletException
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.stereotype.Component
+import org.springframework.web.filter.OncePerRequestFilter
+import org.springframework.web.util.ContentCachingRequestWrapper
+import org.springframework.web.util.ContentCachingResponseWrapper
+import java.io.IOException
+import java.nio.charset.StandardCharsets
+import java.util.*
+
+/**
+ * HTTP 요청 및 응답을 로깅하는 필터
+ *
+ * OncePerRequestFilter를 상속하여 각 요청당 한 번만 실행되도록 보장
+ */
+@Component
+class LoggingFilter : OncePerRequestFilter() {
+    @Throws(ServletException::class, IOException::class)
+    override fun doFilterInternal(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        filterChain: FilterChain,
+    ) {
+        val requestWrapper = ContentCachingRequestWrapper(request)
+        val responseWrapper = ContentCachingResponseWrapper(response)
+
+        val traceId = UUID.randomUUID().toString()
+        responseWrapper.setHeader(TRACE_ID, traceId)
+
+        val startTime = System.currentTimeMillis()
+
+        try {
+            filterChain.doFilter(requestWrapper, responseWrapper)
+        } finally {
+            val duration = System.currentTimeMillis() - startTime
+            logRequestResponse(requestWrapper, responseWrapper, duration, traceId)
+
+            responseWrapper.copyBodyToResponse()
+        }
+    }
+
+    private fun logRequestResponse(
+        request: ContentCachingRequestWrapper,
+        response: ContentCachingResponseWrapper,
+        duration: Long,
+        traceId: String?,
+    ) {
+        val ip = request.getRemoteAddr()
+        val method = request.getMethod()
+        val url = request.getRequestURL().toString()
+        val requestBody = getContent(request.getContentAsByteArray())
+        val status = response.getStatus()
+        val responseBody = getContent(response.getContentAsByteArray())
+
+        logger.info(
+            "[API] traceId: {$traceId}, ip: {$ip}, method: {$method}, url: {$url}, status: {$status}, latency: {$duration}ms request: {$requestBody}, response: {$responseBody}",
+        )
+    }
+
+    private fun getContent(content: ByteArray): String {
+        if (content.size > 0) {
+            return String(content, StandardCharsets.UTF_8)
+        }
+        return "[EMPTY]"
+    }
+
+    companion object {
+        private const val TRACE_ID = "X-Trace-Id"
+    }
+}
